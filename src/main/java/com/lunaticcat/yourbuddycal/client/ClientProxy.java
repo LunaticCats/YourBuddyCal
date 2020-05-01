9d82550609548503441251fb638f3bff2a3371b4
LunaticCat <yannbrun1507@outlook.fr>
2020-05-01 12:09:02 +0200

    Rendering ClassicCalModel

--- src/main/java/com/lunaticcat/yourbuddycal/client/ClientProxy.java
+++ src/main/java/com/lunaticcat/yourbuddycal/client/ClientProxy.java
@@ -1,4 +1,5 @@
 package com.lunaticcat.yourbuddycal.client;
+import com.lunaticcat.yourbuddycal.client.model.ClassicCalModel;
 import com.lunaticcat.yourbuddycal.client.renderer.entity.YourBuddyCalEntityRenderer;
 import com.lunaticcat.yourbuddycal.client.settings.YBCKeyHandler;
 import com.lunaticcat.yourbuddycal.entity.cals.ClassicCalEntity;
@@ -9,7 +10,7 @@ public class ClientProxy
 
     public static void init()
     {
-        RenderingRegistry.registerEntityRenderingHandler(ClassicCalEntity.class, manager -> new YourBuddyCalEntityRenderer<>(manager, new ClassicCalEntity<>(), 2.8F));
+        RenderingRegistry.registerEntityRenderingHandler(ClassicCalEntity.class, manager -> new YourBuddyCalEntityRenderer<>(manager, new ClassicCalModel<>(), 2.8F));
         YBCKeyHandler.registerKeys();
     }
 }
--- src/main/java/com/lunaticcat/yourbuddycal/client/model/ClassicCalModel.java
+++ src/main/java/com/lunaticcat/yourbuddycal/client/model/ClassicCalModel.java
@@ -3,12 +3,14 @@ package com.lunaticcat.yourbuddycal.client.model;
 // Exported for Minecraft version 1.14
 // Paste this class into your mod and generate all required imports
 
+import com.lunaticcat.yourbuddycal.entity.cals.ClassicCalEntity;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.entity.model.RendererModel;
 import net.minecraft.client.renderer.model.ModelBox;
-import net.minecraft.entity.Entity;
+import net.minecraft.util.math.MathHelper;
+import org.lwjgl.opengl.GL11;
 
-class ClassicCalModel extends EntityModel {
+public class ClassicCalModel<T extends ClassicCalEntity> extends EntityModel<T> {
 	private final RendererModel head;
 	private final RendererModel body;
 	private final RendererModel rightarm;
@@ -22,6 +24,7 @@ class ClassicCalModel extends EntityModel {
 
 		head = new RendererModel(this);
 		head.setRotationPoint(3.0F, 10.0F, 4.0F);
+		setRotation(head, 0.0F, 180.0F, 0.0F);
 		head.cubeList.add(new ModelBox(head, 37, 25, -5.0F, -14.0F, -4.0F, 9, 2, 7, 0.0F, false));
 		head.cubeList.add(new ModelBox(head, 44, 34, -5.0F, -13.0F, -7.0F, 9, 1, 3, 0.0F, false));
 		head.cubeList.add(new ModelBox(head, 0, 45, -5.0F, -12.0F, -4.0F, 9, 5, 7, 0.0F, false));
@@ -30,11 +33,13 @@ class ClassicCalModel extends EntityModel {
 
 		body = new RendererModel(this);
 		body.setRotationPoint(2.0F, 24.0F, 6.0F);
+		setRotation(body, 0.0F, 180.0F, 0.0F);
 		body.cubeList.add(new ModelBox(body, 0, 0, -5.0F, -14.0F, -6.0F, 11, 25, 7, 0.0F, false));
 		body.cubeList.add(new ModelBox(body, 62, 25, -4.0F, 11.0F, -4.0F, 9, 3, 3, 0.0F, false));
 
 		rightarm = new RendererModel(this);
 		rightarm.setRotationPoint(-5.0F, 12.0F, 4.0F);
+		setRotation(rightarm, 0.0F, 180.0F, 0.0F);
 		rightarm.cubeList.add(new ModelBox(rightarm, 0, 21, -2.0F, -2.0F, -4.0F, 4, 4, 7, 0.0F, false));
 		rightarm.cubeList.add(new ModelBox(rightarm, 0, 20, -3.0F, 2.0F, -4.0F, 4, 5, 7, 0.0F, false));
 		rightarm.cubeList.add(new ModelBox(rightarm, 36, 0, -2.0F, 7.0F, -2.0F, 2, 11, 3, 0.0F, false));
@@ -46,7 +51,7 @@ class ClassicCalModel extends EntityModel {
 
 		leftarm = new RendererModel(this);
 		leftarm.setRotationPoint(10.0F, 12.0F, 3.0F);
-		setRotationAngle(leftarm, 0.0F, 180.0F, 0.0F);
+		setRotation(leftarm, 0.0F, 180.0F, 0.0F);
 		leftarm.cubeList.add(new ModelBox(leftarm, 0, 21, -2.0F, -2.0F, -4.0F, 4, 4, 7, 0.0F, false));
 		leftarm.cubeList.add(new ModelBox(leftarm, 0, 20, -3.0F, 2.0F, -4.0F, 4, 5, 7, 0.0F, false));
 		leftarm.cubeList.add(new ModelBox(leftarm, 46, 0, -2.0F, 7.0F, -2.0F, 2, 11, 3, 0.0F, false));
@@ -58,7 +63,7 @@ class ClassicCalModel extends EntityModel {
 
 		leftleg = new RendererModel(this);
 		leftleg.setRotationPoint(5.0F, 27.0F, 3.0F);
-		setRotationAngle(leftleg, 0.0F, 180.0F, 0.0F);
+		setRotation(leftleg, 0.0F, 180.0F, 0.0F);
 		leftleg.cubeList.add(new ModelBox(leftleg, 56, 0, -2.0F, 11.0F, -2.0F, 2, 11, 3, 0.0F, false));
 		leftleg.cubeList.add(new ModelBox(leftleg, 69, 31, -2.0F, 22.0F, -3.0F, 2, 8, 3, 0.0F, false));
 		leftleg.cubeList.add(new ModelBox(leftleg, 79, 31, -2.0F, 30.0F, -4.0F, 2, 8, 3, 0.0F, false));
@@ -69,7 +74,7 @@ class ClassicCalModel extends EntityModel {
 
 		rightleg = new RendererModel(this);
 		rightleg.setRotationPoint(-2.0F, 27.0F, 3.0F);
-		setRotationAngle(rightleg, 0.0F, 180.0F, 0.0F);
+		setRotation(rightleg, 0.0F, 180.0F, 0.0F);
 		rightleg.cubeList.add(new ModelBox(rightleg, 66, 0, -2.0F, 11.0F, -2.0F, 2, 11, 3, 0.0F, false));
 		rightleg.cubeList.add(new ModelBox(rightleg, 76, 14, -2.0F, 22.0F, -3.0F, 2, 8, 3, 0.0F, false));
 		rightleg.cubeList.add(new ModelBox(rightleg, 76, 3, -2.0F, 30.0F, -4.0F, 2, 8, 3, 0.0F, false));
@@ -80,18 +85,31 @@ class ClassicCalModel extends EntityModel {
 	}
 
 	@Override
-	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-		head.render(f5);
-		body.render(f5);
-		rightarm.render(f5);
-		leftarm.render(f5);
-		leftleg.render(f5);
-		rightleg.render(f5);
+	public void render(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+
+		scale*=1.5F;
+		GL11.glTranslatef(0F, -0.8F, 0F);
+
+		setRotationAngles(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+		head.render(scale);
+		body.render(scale);
+		rightarm.render(scale);
+		leftarm.render(scale);
+		leftleg.render(scale);
+		rightleg.render(scale);
 	}
 
-	public void setRotationAngle(RendererModel modelRenderer, float x, float y, float z) {
-		modelRenderer.rotateAngleX = x;
-		modelRenderer.rotateAngleY = y;
-		modelRenderer.rotateAngleZ = z;
+	private void setRotation(RendererModel model, float x, float y, float z)
+	{
+		model.rotateAngleX = x;
+		model.rotateAngleY = y;
+		model.rotateAngleZ = z;
+	}
+
+	@Override
+	public void setRotationAngles(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor)
+	{
+		this.leftleg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		this.rightleg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
 	}
 }
